5bf446aae723d04d0a2da72f50d0e9575ebd2cb4
Anzako <antektop@onet.eu>
2024-04-12 17:19:49 +0200

    Scalar field movement
    Redesigned movement controller: movemnt depends on scalar field value, no more raycasts.

--- Assets/Scripts/General/MovementController.cs
+++ Assets/Scripts/General/MovementController.cs
@@ -3,55 +3,40 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    [SerializeField] private LayerMask groundMask;
-    private int layerMask;
-
-    private RaycastHit groundHit;
-    private bool isGrounded;
-    private float maxAngle = 5f;
+    private MetaBalls metaballs;
+    public static float toGroundPotential = 0.47f;
+    //private float maxAngle = 5f;
 
     public float moveSpeed;
-    public float toGroundDistance;
-    public float interpolationSpeed;
+    //public float toGroundDistance;
+    public static float gravityForce = 4f;
     public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        layerMask = 1 << 7;
+        metaballs = MetaBalls.instance;
     }
 
     // Update is called once per frame
     public void MovementUpdate(float horizontalRotationAngle)
     {
         RotateAroundVerticalAxis(horizontalRotationAngle);
+
     }
 
     public void MovementFixedUpdate(Vector2 moveDirection)
     {
-        //Debug.DrawRay(transform.position, -transform.up, Color.green);
-        //Debug.DrawRay(transform.position, -transform.forward, Color.red);
-        RotateToGround();
+        RotateToSurface();
         Move(moveDirection.normalized);
+
     }
 
     private void Move(Vector2 moveDirection)
     {
-        if (!isGrounded) return;
-        
-        /*Vector3 horizontalVector = -Vector3.Cross(transform.forward, transform.up) * moveDirection.x;
-        Vector3 verticalVector = transform.forward * moveDirection.y;
-        Vector3 projectedVector = horizontalVector + verticalVector;
-        projectedVector.Normalize();
+        Vector3 projectedVector = transform.forward * moveDirection.y + transform.right * moveDirection.x;
 
-        transform.position += projectedVector * moveSpeed * Time.deltaTime;*/
-
-        Vector3 horizontalVector = Vector3.ProjectOnPlane(transform.forward, transform.up) * moveDirection.y;
-        Vector3 projectedVector = horizontalVector + transform.right * moveDirection.x;
-
-        // Unikaj wielokrotnego odwo³ywania siê do transform.position
-        Vector3 newPosition = transform.position + projectedVector.normalized * moveSpeed * Time.deltaTime;
-        transform.position = newPosition;
+        transform.position += projectedVector.normalized * moveSpeed * Time.deltaTime;
     }
 
     private void RotateAroundVerticalAxis(float rotationAngle)
@@ -60,59 +45,24 @@ public class MovementController : MonoBehaviour
         transform.rotation = targetRotation * transform.rotation;
     }
 
-    private void RotateToGround()    
+    private void RotateToSurface()    
     {
-        if (Physics.Raycast(transform.position, -transform.up, out groundHit, layerMask))
-        {
-            isGrounded = true;
-            //Debug.DrawRay(transform.position, -transform.up, Color.green);  // Visualize ground normal
+        Vector3 potentialVector = metaballs.CalculateMetaballsNormal(transform.position);
+        Debug.DrawRay(transform.position, potentialVector.normalized, Color.red);
 
-            if (Vector3.Angle(transform.up, groundHit.normal) > maxAngle)
-            {
-                Quaternion targetRotation = Quaternion.FromToRotation(transform.up, groundHit.normal)
+        float scalarValue = metaballs.CalculateScalarFieldValue(transform.position);
+
+        // Rotating object to new rotation depending on potential vector
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, potentialVector.normalized)
                 * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation,
+            Time.deltaTime * rotationSpeed);
 
-                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation,
-                    Time.deltaTime * rotationSpeed);
-            }
-     
-            AddGravity();
-        } else
+        float val = toGroundPotential - scalarValue;
+        if (Math.Abs(val) > 0.01f)
         {
-            isGrounded = false;
+            transform.position -= potentialVector.normalized * val * gravityForce;
         }
-
-        if (IsInsideMetaballs())
-        {
-            PushObjectFromGround();
-        }
-    }
-
-    private void AddGravity() 
-    {
-        float distance = groundHit.distance - toGroundDistance;
-
-        if (Math.Abs(distance) > 0.01)
-        {
-            transform.position = transform.position - transform.up * distance;
-        }
-    }
-
-    private bool IsInsideMetaballs()
-    {
-        // change 0.5f on treshold variable
-
-        return MetaBalls.instance.CalculateScalarFieldValue(transform.position) > 0.5f;
-    }
-
-    private void PushObjectFromGround()
-    {
-        Vector3 vector = MetaBalls.instance.CalculateMetaballsNormal(transform.position);
-        
-        transform.position += vector;
-        transform.up = vector.normalized;
-
-        //Debug.DrawRay(transform.position, vector, Color.green);
     }
 
 }
