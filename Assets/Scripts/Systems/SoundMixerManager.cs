f659c37a2b1295320eccb446c7fd886d3fe338e0
Anzako <antektop@onet.eu>
2024-06-17 10:53:47 +0200

    Add music

--- Assets/Scripts/Systems/SoundMixerManager.cs
+++ Assets/Scripts/Systems/SoundMixerManager.cs
@@ -5,5 +5,20 @@ using UnityEngine.Audio;
 
 public class SoundMixerManager : MonoBehaviour
 {
-    
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float level)
+    {
+
+    }
+
+    public void SetSoundFXVolume(float level)
+    {
+
+    }
+
+    public void SetMusicVolume(float level)
+    {
+
+    }
 }
